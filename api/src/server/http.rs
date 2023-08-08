937b8af0cc5696d89bad95549c5e34a4a1f80020
Vitor Livi <vitorlivi1999@gmail.com>
2023-08-08 13:24:40 -0300

    feat: create websocket

--- api/src/server/http.rs
+++ api/src/server/http.rs
@@ -4,11 +4,12 @@ use crate::server::websocket::websocket;
 use actix_web::{web, App, HttpServer};
 
 pub async fn http_server() -> std::io::Result<()> {
-    return HttpServer::new(|| 
-                           App::new().service(hello)
-                            .service(web::resource("/ws").route(web::get().to(websocket)))
-                           )
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await;
+    return HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(web::resource("/ws").route(web::get().to(websocket)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await;
 }
--- api/src/server/websocket.rs
+++ api/src/server/websocket.rs
@@ -1,5 +1,5 @@
 use actix::{Actor, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
 /// Define HTTP actor
@@ -12,17 +12,33 @@ impl Actor for MyWs {
 /// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        println!("{:?}", msg);
+
         match msg {
             Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Text(text)) => ctx.text(handleMessage(text)),
             Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
             _ => (),
         }
     }
 }
 
+fn handleMessage(msg: String) -> _ {
+    onJoin(msg.as_str());
+}
+
+fn onJoin(msg: &String) -> _ {
+    let mut msg = msg;
+
+    if msg.contains("join:") {
+        msg = msg.replace("join:", "");
+        println!("Joining room: {}", msg);
+    }
+}
+
 pub async fn websocket(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     let resp = ws::start(MyWs {}, &req, stream);
+
     println!("{:?}", resp);
     resp
 }
